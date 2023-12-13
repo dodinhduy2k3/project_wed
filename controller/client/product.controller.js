c9671ca749ca21a1a2c0c916a9da602dac01cf26
dodinhduy2k3 <dodinhduy203@gmail.com>
2023-12-13 18:34:15 +0700

    test mongo

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,11 @@
-module.exports.index = (req,res)=>{
+const Product = require('../../model/product.model')
+module.exports.index = async (req,res)=>{
+    const products = await Product.find({})
+    // console.log(products)
     res.render("client/pages/products/index.pug",{
-        pagetitle : "Trang Danh Sách Sản Phẩm "
+        pagetitle : "Trang Danh Sách Sản Phẩm ",
+        products: products
+
     });
 
 }
